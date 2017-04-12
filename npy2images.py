af8be391f80dfc43f07d5065f39bba34351e3217
da7th-linux <m.da7th@gmail.com>
2017-04-12 09:59:12 +0300

    fix - offset implementation in npy2image extraction fixed

--- npy2images.py
+++ npy2images.py
@@ -46,7 +46,7 @@ def extract_cropped_images():
         #Image setup, plot, and save as full frame under the bag file's name
         fig = plt.figure(frameon = False, figsize=(15,15))
         cm = plt.cm.get_cmap('Spectral')
-        sc = plt.scatter(x_s, y_s, c=z_s, s=3, edgecolors= '', cmap=cm)
+        sc = plt.scatter(x_s, y_s, c=z_s, s=.5, edgecolors= '', cmap=cm)
         # plt.axis('square')
         ax = plt.gca()
         ax.axes.get_xaxis().set_visible(False)
@@ -88,7 +88,7 @@ def extract_cropped_images():
         for x_step in range(np.int(xsize/step)):
             for y_step in range(np.int(ysize/step)):
                 cropped_img_count += 1
-                cropped_img = croppable_img[offset+step*x_step:step*x_step+step , offset+step*y_step:step*y_step+step]
+                cropped_img = croppable_img[offset+step*x_step:offset+step*x_step+step , offset+step*y_step:offset+step*y_step+step]
                 imgdirectory = directory + "/frame_" + str(img_count)
                 if not os.path.exists(imgdirectory):
                     os.makedirs(imgdirectory)
