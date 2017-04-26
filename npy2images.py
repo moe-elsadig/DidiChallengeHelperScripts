b4e8d5fd417a065ce279213cbf4a9485e015073c
Moe Elsadig <m.da7th@gmail.com>
2017-04-26 15:12:13 +0300

    fix - npy2images.py
    increased dpi and marker size

--- npy2images.py
+++ npy2images.py
@@ -9,12 +9,16 @@ import argparse
 cwd = os.getcwd()
 file_name = "approach_1.bag_format_XYZIR.npy"
 
+# Extraction function
 def extract_cropped_images():
     #load all the frames
     print "Loading all frames..."
+
+    #load all the frames in the given npy file
     all_frames = np.asarray(np.load(cwd + "/" + file_name))
     print "Frames loaded.\nExtracting images..."
     img_count = 0
+    #iterate through the frames and limit all x and y values to the range -25 to 25
     for frame in all_frames:
         img_count += 1
         #Extract the points
@@ -35,18 +39,21 @@ def extract_cropped_images():
         del_indeces = []
         del_count = 0
 
+        #limit the points on the frame based on height (enough for pedestrians and vehicles)
+        #create a list of indeces corresponding to points outside the height range
         for i in range(len(z_s)):
             if z_s[i] > 0.8 or z_s[i] < -1.30:
                 del_indeces.append(i-del_count)
                 del_count += 1
 
+        #delete the incdeces obtained above
         for index in del_indeces:
             del z_s[index], x_s[index], y_s[index], i_s[index], r_s[index]
 
         #Image setup, plot, and save as full frame under the bag file's name
         fig = plt.figure(frameon = False, figsize=(15,15))
         cm = plt.cm.get_cmap('Spectral')
-        sc = plt.scatter(x_s, y_s, c=z_s, s=.5, edgecolors= '', cmap=cm)
+        sc = plt.scatter(x_s, y_s, c=z_s, s=3, edgecolors= '', cmap=cm)
         # plt.axis('square')
         ax = plt.gca()
         ax.axes.get_xaxis().set_visible(False)
@@ -56,11 +63,12 @@ def extract_cropped_images():
         ax.set_ylim([-30,30])
         # plt.colorbar(sc)
 
+        #directory for the images to be saved in
         directory = cwd + "/" + file_name[:5] + "extraction"
         if not os.path.exists(directory):
             os.makedirs(directory)
 
-        plt.savefig((directory + "/img_" + str(img_count) + ".png"), bbox_inches='tight', dpi=200, pad_inches=0.0)
+        plt.savefig((directory + "/img_" + str(img_count) + ".png"), bbox_inches='tight', dpi=300, pad_inches=0.0)
 
         #Load the image again for cropping
         croppable_img = cv2.imread(directory + "/img_" + str(img_count) + ".png")
